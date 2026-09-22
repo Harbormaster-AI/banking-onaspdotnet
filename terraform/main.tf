3777f71067b26f901c7a51762e2975629225f6d1
Ethan Williams <ethan.williams@mobilefirst.app>
2026-09-22 09:17:23 -0400

    Initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
       CreatedBy = "Harbormaster"
       Blueprint = "ASP.NET-10"
       DomainModel = "Banking Industry Domain Model"
-      CertificationId = "104387f9-e41b-4cd5-87c9-c396e5d3ce52"
+      CertificationId = "e8571f50-5aba-4b20-887c-2f87789e2ca5"
     }
   }
 }
@@ -39,7 +39,7 @@ resource "local_file" "private_key_pem" {
 }
 
 resource "aws_key_pair" "generated" {
-  key_name   = "pjsk-sshtest-0.2706570956692602"
+  key_name   = "pjsk-sshtest-0.21193910842622155"
   public_key = tls_private_key.generated.public_key_openssh
 
   lifecycle {
